50559b137d74bbf3f1b2967749600740819eec6b
Ismael G <underthehaze13@outlook.com>
2024-01-29 09:47:04 -0800

    created the map using d3.geo path and the API information

--- index.js
+++ index.js
@@ -1,14 +1,52 @@
 // URL for the API 
-let URL = ['https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/for_user_education.json', 'https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/counties.json']
+let educationURL = 'https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/for_user_education.json'
+let countyURL = 'https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/counties.json'
 
+let countyData
+let educationData
 
+let width = 1200;
+let height = 600
+let padding = 60;
 
 //create the canvas 
 
-
-
-//create the mapo
+let canvas = d3.select('#canvas')
 
 
 
-//call the API 
+//create the map for county and US
+createMap = () =>{
+    canvas.selectAll('path')
+            .data(countyData)
+            .enter()
+            .append('path')
+            .attr('d', d3.geoPath())
+            .attr('class', 'county')
+
+
+
+
+}
+
+
+//call the API for education and county
+d3.json(countyURL).then((data,error)=>{
+    if (error){
+        console.log(log)
+    }else{
+        countyData = topojson.feature(data, data.objects.counties).features
+        console.log(countyData)
+        d3.json(educationURL).then((data,error)=> {
+            if(error){
+                console.log(log)
+
+            }else {
+                educationData = data
+                console.log(educationData)
+                createMap()
+            }
+        })
+    }
+}
+)
