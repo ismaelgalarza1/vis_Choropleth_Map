7da99be4eb44bfa7bf9e9c71073244de110c9a96
Ismael G <underthehaze13@outlook.com>
2024-01-29 09:15:04 -0800

    created the project outline, and started the javascript variable outlines

---
+++ index.js
@@ -0,0 +1,14 @@
+// URL for the API 
+let URL = ['https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/for_user_education.json', 'https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/counties.json']
+
+
+
+//create the canvas 
+
+
+
+//create the mapo
+
+
+
+//call the API 
